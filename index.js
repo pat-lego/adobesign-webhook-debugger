6ac15551cf4d6f85a9a26180990c898d90ec52db
pat-lego <patrique.legault@gmail.com>
2021-10-08 15:26:39 -0400

    Logging event examples

--- index.js
+++ index.js
@@ -6,26 +6,19 @@ const port = 3000
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    console.log("About to log the GET body request")
-    console.log(req.body)
+    console.log(`The X-AdobeSign-ClientId request header is set too: ${req.header('X-AdobeSign-ClientId')}`)
+    console.log()
+    res.set('X-AdobeSign-ClientId',req.header('X-AdobeSign-ClientId'))
     res.sendStatus(200)
 })
 
 app.post('/', (req, res) => {
-    console.log("About to log the POST body request")
+    console.log('About to log the body of the request')
     console.log(req.body)
-    res.sendStatus(200)
-})
 
-app.put('/', (req, res) => {
-    console.log("About to log the PUT body request")
-    console.log(req.body)
-    res.sendStatus(200)
-})
-
-app.delete('/', (req, res) => {
-    console.log("About to log the DELETE body request")
-    console.log(req.body)
+    console.log(`The X-AdobeSign-ClientId request header is set too: ${req.header('X-AdobeSign-ClientId')}`)
+    console.log()
+    res.set('X-AdobeSign-ClientId',req.header('X-AdobeSign-ClientId'))
     res.sendStatus(200)
 })
 
