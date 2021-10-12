de5c0b5718b081986c3c11597b4226693fb92806
pat-lego <patrique.legault@gmail.com>
2021-10-12 13:54:29 -0400

    Adding better logging

--- README.md
+++ README.md
@@ -7,11 +7,19 @@ An Express server to debug Adobe Sign webhooks
 1. Install ngrok locally on your laptop (i.e. https://ngrok.com/download)
 2. Make sure you have node installed (i.e. https://nodejs.org/en/download/)
 3. In the root of the project run `npm install`
-4. In the root of the project run `node index.js`
+4. In the root of the project run `npm run debug`
     - Now you have a server that is accessible at http://localhost:3000
 5. Execute `ngrok http 3000`
     - Locate the public facing URL from the command result
 6. Create an Adobe Sign Webhook with the resulting ngrok URL (i.e. https://helpx.adobe.com/sign/using/adobe-sign-webhooks-api.html)
+7. View the webhook events being logged in the sign-webhook.log file (located at the root of this repository)
+    - **Note:** The sign-webhook.log file will only appear once step 4 is executed
+
+## Document Review
+
+The PDF can be viewed by locating the Base64 string representing the document at this JSONPath `agreement.signedDocumentInfo.document`.
+
+**Note:** For quick testing purposes users can copy paste the string and view the PDF here https://base64.guru/converter/decode/pdf
 
 ## Known Issues
 
--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const { response } = require('express')
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json())
+app.use(express.json({ extended: false, limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }))
+
+const logFile = 'sign-webhook.log';
 
 app.get('/', (req, res) => {
     console.log(`The X-AdobeSign-ClientId request header is set too: ${req.header('X-AdobeSign-ClientId')}`)
     console.log()
-    res.set('X-AdobeSign-ClientId',req.header('X-AdobeSign-ClientId'))
+
+    res.set('X-AdobeSign-ClientId', req.header('X-AdobeSign-ClientId'))
     res.sendStatus(200)
 })
 
 app.post('/', (req, res) => {
-    console.log('About to log the body of the request')
-    console.log(req.body)
+    console.log(`Received the JSON payload for agreement id ${req.body.agreement.id} - name: ${req.body.agreement.name}`)
+
+    fs.writeFile(logFile, `${JSON.stringify(req.body)}\r\n`, { flag: "a+" }, (error) => {
+        if (error) {
+            console.error(`Received the following error message ${error.name} when trying to write to ${logFile}`)
+        }
+    })
 
     console.log(`The X-AdobeSign-ClientId request header is set too: ${req.header('X-AdobeSign-ClientId')}`)
     console.log()
-    res.set('X-AdobeSign-ClientId',req.header('X-AdobeSign-ClientId'))
+    res.set('X-AdobeSign-ClientId', req.header('X-AdobeSign-ClientId'))
     res.sendStatus(200)
 })
 
